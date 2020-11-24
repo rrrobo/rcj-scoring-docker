e3d6346a9c3f5c7f1b38e875b76815da692e719b
rrrobo <rrrobo@icloud.com>
2020-11-24 19:48:59 +0900

    add mailTemplates folder

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ mkdir -p logs
 mkdir -p db
 mkdir -p documents
 mkdir -p fonts
+mkdir -p mailTemplates
 echo "最新版のイメージをDocker Hubよりダウンロードします"
 sudo docker pull ryorobo/rcj-rescue-scoring-japan:latest
 echo "イメージを起動します"
