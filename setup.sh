37ce772ea36f0fbcdbad52ec8ea1968c68ee8dc2
rrrobo <rrrobo@icloud.com>
2020-12-03 16:31:17 +0900

    add default templates

--- setup.sh
+++ setup.sh
@@ -4,7 +4,6 @@ mkdir -p logs
 mkdir -p db
 mkdir -p documents
 mkdir -p fonts
-mkdir -p templates
 echo "最新版のイメージをDocker Hubよりダウンロードします"
 sudo docker pull ryorobo/rcj-rescue-scoring-japan:latest
 echo "イメージを起動します"
